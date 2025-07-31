35b58a95c2f8eb478453b8980dec8069291ad34e
Nihar Patel <niharpatel52@gmail.com>
2025-07-31 14:30:24 -0400

    create adoption methods

--- app/models/adoption.rb
+++ app/models/adoption.rb
@@ -2,5 +2,13 @@ class Adoption < ActiveRecord::Base
   belongs_to :adopter
   belongs_to :shelter
 
+  def adopted_pet?(pet_name)
+    Adoption.exists?(adopter_id: self.id, pet_name: pet_name)
+  end
 
+  def transfer_adoption(new_adopter, adoption)
+    if adoption.adopter_id == self.id
+      adoption.update(adopter_id: new_adopter.id)
+    end
+  end
 end
