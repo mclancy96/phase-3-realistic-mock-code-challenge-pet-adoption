cf68949994747e14ed3f3ca51a2d54a3332ef769
Nihar Patel <niharpatel52@gmail.com>
2025-07-31 14:27:40 -0400

    create adopter methods

--- app/models/adopter.rb
+++ app/models/adopter.rb
@@ -2,5 +2,11 @@ class Adopter < ActiveRecord::Base
   has_many :adoptions
   has_many :shelters, through: :adoptions
 
+  def shelter
+    Shelter.find(self.shelter_id)
+  end
 
+  def adopter
+    Adopter.find(self.adopter_id)
+  end
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,4 +2,16 @@ class Shelter < ActiveRecord::Base
   has_many :adoptions
   has_many :adopters, through: :adoptions
 
+  def process_adoption(adopter, pet_name, adoption_fee)
+  Adoption.create(
+    adopter_id: adopter.id,
+    shelter_id: self.id,
+    pet_name: pet_name,
+    adoption_fee: adoption_fee
+  )
+  end
+
+  def self.oldest_shelter
+    Shelter.order(founded_year: :asc).first
+  end
 end
